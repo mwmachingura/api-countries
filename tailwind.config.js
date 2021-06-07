f47977a7e1d3acda749834200c5919e09d08029d
mwmachingura <mutsamaching@gmail.com>
2021-06-07 10:23:52 +0200

    Extended Colors In Tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,16 @@ module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'dark-blue': 'hsl(209, 23%, 22%)', //Dark Mode Elements
+        'very-dark-blue': 'hsl(207, 26%, 17%)', //Dark Mode Background
+        'dark-blue-text': 'hsl(200, 15%, 8%)', //Light Mode Text
+        'dark-gray': 'hsl(0, 0%, 52%)', //Light Mode Input
+        'very-light-gray': 'hsl(0, 0%, 98%)', //Light Mode Background
+        'white': 'hsl(0, 0%, 100%)', //Dark Mode Text & Light Mode Elements
+      }
+    },
   },
   variants: {
     extend: {},
