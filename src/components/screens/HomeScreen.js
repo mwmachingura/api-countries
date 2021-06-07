7337e648bd385f26dd82dfaf7568d86398e3490d
mwmachingura <mutsamaching@gmail.com>
2021-06-07 12:05:24 +0200

    useDispatch and useSelector Implemented. API Populating HomeScreen

--- src/components/screens/HomeScreen.js
+++ src/components/screens/HomeScreen.js
@@ -1,10 +1,27 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Menu } from '@headlessui/react';
+import { useDispatch, useSelector } from 'react-redux';
+
 
 
 import Countries from '../Countries/Countries'
+import { getCountries, getCountriesByRegion } from '../../actions/countries';
 
 const HomeScreen = () => {
+
+    const dispatch = useDispatch();
+    
+    const [region, setRegion] = useState(null);
+
+    useEffect(() => {
+        if (region) return dispatch(getCountriesByRegion(region));
+        return dispatch(getCountries());
+    }, [dispatch, region]);
+
+    const countries = useSelector((state) => state.countries);
+
+    console.log(countries);
+
     return (
         <>
             <div>
@@ -15,19 +32,44 @@ const HomeScreen = () => {
                     <Menu.Button>Filter By Region</Menu.Button>
                     <Menu.Items>
                         <Menu.Item>
-                            <button>Africa</button>
+                            <button onClick={(e) => {
+                                e.preventDefault();
+                                setRegion('Africa')
+                            }}>
+                                Africa
+                            </button>
                         </Menu.Item>
                         <Menu.Item>
-                            <button>Americas</button>
+                        <button onClick={(e) => {
+                                e.preventDefault();
+                                setRegion('Americas')
+                            }}>
+                                Americas
+                            </button>
                         </Menu.Item>
                         <Menu.Item>
-                            <button>Asia</button>
+                        <button onClick={(e) => {
+                                e.preventDefault();
+                                setRegion('Asia')
+                            }}>
+                                Asia
+                            </button>
                         </Menu.Item>
                         <Menu.Item>
-                            <button>Europe</button>
+                        <button onClick={(e) => {
+                                e.preventDefault();
+                                setRegion('Europe')
+                            }}>
+                                Europe
+                            </button>
                         </Menu.Item>
                         <Menu.Item>
-                            <button>Oceania</button>
+                        <button onClick={(e) => {
+                                e.preventDefault();
+                                setRegion('Oceania')
+                            }}>
+                                Oceania
+                            </button>
                         </Menu.Item>
                     </Menu.Items>
                 </Menu>
