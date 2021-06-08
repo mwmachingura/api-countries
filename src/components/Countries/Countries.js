41205e1bdaf9a6ab0823014a7e89c6e02891bf45
mwmachingura <mutsamaching@gmail.com>
2021-06-08 16:48:41 +0200

    Created Country Component With Dark Mode Styling

--- src/components/Countries/Countries.js
+++ src/components/Countries/Countries.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import Country from './Country/Country';
 
 
 const Countries = ({ countries, dark }) => {
     return (
-        <div className='py-4 px-2 h-full'>
-            <div className='grid grid-col-1 gap-y-4 justify-items-center'>
+        <div className='py-4 px-2 h-full flex justify-center'>
+            <div className='grid grid-col-1 gap-y-4 justify-items-center w-72'>
                 {countries.map((country) => (
                     <Link to={`/${country.name}`} className='cursor-pointer outline-none focus:outline-none w-full h-full'>
-                        <div className='bg-green-300 w-full max-w-xs h-48 overflow-hidden rounded' key={country.name} id={country.name}>
-                            Hello Mr Light
+                        <div className='w-full overflow-hidden rounded flex justify-center' key={country.name} id={country.name}>
+                            <Country country={country} dark={dark} />
                         </div>
                     </Link>
                 ))}
---
+++ src/components/Countries/Country/Country.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Country = ({ country, dark }) => {
+    return (
+        <div className={`w-72 overflow-hidden shadow-lg h-auto ${(dark) ? 'bg-dark-blue text-white': 'bg-white text-dark-blue-text'}`}>
+            <div className='w-full'>
+                <img alt='flag' src={country.flag} className='w-full object-cover h-36' />
+            </div>
+            <div className='h-40 px-2'>
+                <h3 className='text-xl font-bold py-2'>{country.name}</h3>
+                <div className='flex flex-col items-start'>
+                    <h4 className='font-semibold text-sm py-1'>Population: <span className='font-light'>{country.population}</span></h4>
+                    <h4 className='font-semibold text-sm py-1'>Region: <span className='font-light'>{country.region}</span></h4>
+                    <h4 className='font-semibold text-sm py-1'>Capital: <span className='font-light'>{country.capital}</span></h4>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Country
