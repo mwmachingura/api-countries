9c5d1f0656ad568442f2bc5427063a8c0aba5ee8
Mutsa W. Machingura <mutsamaching@gmail.com>
2022-05-25 11:39:07 +0200

    API url updated

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const url = 'https://restcountries.eu/rest/v2';
+const url = 'https://restcountries.com/v2';
 
 export const fetchCountries = () => axios.get(`${url}/all`);
 export const fetchCountriesByRegion = (region) => axios.get(`${url}/region/${region}`);
